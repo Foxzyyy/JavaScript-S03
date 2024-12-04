4b5e221bac8c676d099227f2848230dde1f3a928
Foxzyyy <sonyeonnisn@gmail.com>
2024-12-04 10:02:17 +0700

    .

---
+++ implecit-false.js
@@ -0,0 +1,6 @@
+const name ='';
+if (name){
+    console.log('We have a nbame!');
+} else {
+    console.log('No name provied');
+}
---
+++ nummath.js
@@ -0,0 +1,18 @@
+let num1 = 100;
+//basic math
+console.log("*********Basic Math**********");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+
+console.log("*********Additional Mathematical Operation*********");
+//Addition Mathematical Operations
+console.log(num1 % 1500); //remaider
+console.log(++num1); //Increment
+console.log(--num1); //Decrement
+
+console.log("*********8Using the math objects********");
+//using the math object
+console.log(Math.PI); //pi
+console.log(Math.sqrt(num1)); //square root
